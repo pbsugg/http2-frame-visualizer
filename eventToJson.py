d5959e8f2f709f1f61f66031ac7a2bd12891d874
Philip Sugg <pbensugg@gmail.com>
2016-03-22 00:06:04 -0500

    add wrapper method for parsing events into JSON

--- eventToJson.py
+++ eventToJson.py
@@ -10,27 +10,30 @@ import json
 # sort the object by type
 # give it to the method with the right encoding method
 
-class FrameEventToJSON(json.JSONEncoder):
+class FrameEventToJSON():
 
     def default(self, event):
 
         if isinstance(event, RequestReceived):
-            return {"stream_id": event.stream_id,
+            return  {"type": "RequestReceived", "stream_id": event.stream_id,
                     "headers": event.headers}
         elif isinstance(event, DataReceived):
-            return {"stream_id": event.stream_id,
-            "flow_controlled_length": event.flow_controlled_length,
-            "data": event.data}
+            return {"type": "DataReceived", 
+                    "stream_id": event.stream_id, 
+                    "flow_controlled_length": event.flow_controlled_length,
+                    "data": "Hello"}
         elif isinstance(event, RemoteSettingsChanged):
-            return {"changed_settings": event.changed_settings}
+            return {"type": "RemoteSettingsChanged", 
+                    "changed_settings": event.changed_settings}
         elif isinstance(event, StreamEnded):
-            return {"stream_id":event.stream_id}
+            return {"type": "StreamEnded", "stream_id" : event.stream_id}
         # elif isinstance(event, StreamReset):
         elif isinstance(event, SettingsAcknowledged):
-            return {"changed_settings": event.changed_settings}
+            return {"type": "SettingsAcknowledged", 
+                    "changed_settings": event.changed_settings}
         elif isinstance(event, ResponseReceived):
-            return {"stream_id": event.stream_id,
-            "headers": self.parse_response_headers_to_JSON(event)} 
+            return {"type": "ResponseReceived", "stream_id" : event.stream_id,
+                    "headers": self.parse_response_headers_to_JSON(event)} 
 
     # return the header object as a series of pairs in dictionary 
     def parse_response_headers_to_JSON(self, event):
@@ -40,5 +43,10 @@ class FrameEventToJSON(json.JSONEncoder):
             return_dict[header_pair[0]] = header_pair[1]
         return return_dict
 
-            
+    def packageAllEvents(self, events): 
+        print(events)
+        returnPackage = {}
+        for number, event in enumerate(events):
+            returnPackage[number] = self.default(event) 
+        return json.dumps(returnPackage) 
     
