f2137bec0f02493e9b0838ca802048c0ce4da36d
Philip Sugg <pbensugg@gmail.com>
2016-04-07 18:33:21 -0500

    add working response from redis broken with backend results

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template, request
 import json
 from twisted_client import *
-from celery import Celery
+from celery import Celery, result
 
 app = Flask(__name__)
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379'
 
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery = Celery(app.name, backend=app.config['CELERY_RESULT_BACKEND'], broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
 @celery.task(bind=True)
 def run_web_server():
-    run()
+    ## run()
+    return("hello")
 
 @app.route('/')
 def index():
@@ -21,7 +22,9 @@ def index():
 @app.route('/start')
 def return_client_request():
     task = run_web_server.apply_async()
+    print(task.task_id)
     print(task.status)
+    print(celery.result.AsyncResult(task.task_id))
     return("goodbye")
     
 if __name__ == '__main__':
