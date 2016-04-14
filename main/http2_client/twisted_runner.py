b5abf08b66b5cff3f07de2874cfbce7c6830e1d5
Philip Sugg <pbensugg@gmail.com>
2016-04-14 11:14:33 -0500

    rearrange file structure and make twisted_runner import files from directory one level up

--- main/http2_client/twisted_runner.py
+++ main/http2_client/twisted_runner.py
@@ -5,32 +5,37 @@ fixed ssl with "acceptableProtocols in twisted, see this:"
 http://twisted.readthedocs.org/en/latest/core/howto/ssl.html
 """
 
+import os 
+#twisted imports
 from twisted.internet import reactor
 from twisted.internet.endpoints import connectProtocol, SSL4ClientEndpoint
 from twisted.internet.ssl import optionsForClientTLS
-
-from messageHandler import messageHandler
+# my twisted files
 from twisted_client import H2Protocol
 
-def hello():
-    print("hello")
+# for passing messages
+BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
+os.chdir('..')
+from messageHandler import messageHandler
+os.chdir(BASE_DIR)
+
 
 SIZE = 4096
 AUTHORITY = u'twitter.com'
 PATH = '/'
 
-options = optionsForClientTLS(
-    hostname=AUTHORITY,
-    acceptableProtocols=[b'h2', b'http/1.1'],
-)
+def run(messageHandler): 
 
-handler = messageHandler()
+    options = optionsForClientTLS(
+        hostname=AUTHORITY,
+        acceptableProtocols=[b'h2', b'http/1.1'],
+    )
 
-connectProtocol(
-    SSL4ClientEndpoint(reactor, AUTHORITY, 443, options),
-    H2Protocol(handler)
-)
+    connectProtocol(
+        SSL4ClientEndpoint(reactor, AUTHORITY, 443, options),
+        H2Protocol(handler)
+    )
 
-def run():
     reactor.run(installSignalHandlers=0)
+
 
